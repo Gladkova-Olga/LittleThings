ba7adb79635710ca87f0733f659b6ed3ecf6dff2
Lisla Fox <lisla@Lislas-MBP.home>
2021-05-12 15:03:23 +0300

    useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {useState} from "react";
+
+export default {
+    title:'Use state'
+}
+
+function generateData() {
+    //difficult counting
+    console.log("generate data");
+    return 12345
+}
+
+export const Example1 = () => {
+    console.log("ex1")
+    const [counter, setCounter] = useState(generateData);
+    const changer = (state: number) => state + 1
+    return(
+        <>
+            <button onClick={() => setCounter(changer)}>+</button>
+            {counter}
+        </>
+    )
+ 
+}
